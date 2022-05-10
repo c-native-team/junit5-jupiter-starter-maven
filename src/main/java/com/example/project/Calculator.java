d66429846418979aa379cb231d2d23f4afe83e06
hiroki-endo-1 <103981858+hiroki-endo-1@users.noreply.github.com>
2022-05-10 15:27:08 +0900

    Update Calculator.java

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -18,7 +18,7 @@ public class Calculator {
 	public static void main( String[] args )
 	{
 		while(true){
-			System.out.println("Hello World! Test 2");
+			System.out.println("Hello World! Test Main");
 			try
 			{
 				Thread.sleep(10000);
